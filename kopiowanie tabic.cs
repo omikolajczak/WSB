ba6747f673795675e4356f68cb99c8897caa7d42
omikolajczak <56199380+omikolajczak@users.noreply.github.com>
2019-11-10 17:42:44 +0100

    Add files via upload

---
+++ kopiowanie tabic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kopiowianie_tablic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kopiowanie tablic
+
+            int[] tab = { 1, 2, 3, 4, 5 };
+            int[] tabCopy = new int[7];
+
+            foreach (int item in tabCopy)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+            tab.CopyTo(tabCopy, 0);
+
+
+            foreach (int item in tabCopy)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+            Array.Copy(tab, tabCopy, tab.Length);
+
+            foreach (int item in tabCopy)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ tyb wartosciowy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //typ wartościowy
+            int a = 10;
+            int b = a;
+
+            a--;
+            b++;
+
+            Console.WriteLine(a); //9
+            Console.WriteLine(b); //11
+
+            //typ referencyjny
+
+            int[] tabA = { 1, 2, 3 };
+            int[] tabB = tabA;
+
+            tabA[0] = 100;
+            tabA[1] = 200;
+
+
+            Console.WriteLine("tabA");
+
+            foreach (int item in tabA)
+            {
+                Console.WriteLine("{0} ", item);
+            }
+            Console.WriteLine("\n\ntabB");
+
+            foreach (int item in tabB)
+            {
+                Console.WriteLine("{0} ", item);
+            }
+
+            
+
+            string text1 = "10";
+            string text2 = text1;
+
+            text1 += "Janusz";
+
+            Console.WriteLine(text1); //10Janusz
+            Console.WriteLine(text2); //10
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
